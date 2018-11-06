87442e142d1d4bf6eb5220ada13c532aba42f759
Edoarda Vallim <medoarda@id.uff.br>
2018-11-05 23:23:17 -0200

    Commit inicial

---
+++ t1/t1.c
@@ -0,0 +1,27 @@
+/*
+* AUTORES:
+* Hugo Carneiro
+* Edoarda Vallim
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+#define FUNCAO(t, alpha) (1.5 + sin(t))*alpha;
+//or use inline, idk
+
+float rk2() {
+    float yn1 = 0.0;
+    return yn1;
+}
+
+float bdf2() {
+    float yn1 = 0.0;
+    return yn1;
+}
+
+int main() {
+    float t;
+    float alpha;
+    float fun = (1.5 + sin(t))*alpha;
+}
